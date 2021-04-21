240400ad735b67e22808a752af2433cb4a70a0e5
vineethvijayan <vineeth.v@naicoits.com>
2021-04-21 23:39:19 +0530

    Working Code

--- app/navigation/main-navigator.tsx
+++ app/navigation/main-navigator.tsx
@@ -6,7 +6,7 @@
  */
 import React from "react"
 import { createStackNavigator } from "@react-navigation/stack"
-import { WelcomeScreen, DemoScreen } from "../screens"
+import { QuestionScreen } from "../screens"
 
 /**
  * This type allows TypeScript to know what routes are defined in this navigator
@@ -35,8 +35,7 @@ export function MainNavigator() {
         headerShown: false,
       }}
     >
-      <Stack.Screen name="welcome" component={WelcomeScreen} />
-      <Stack.Screen name="demo" component={DemoScreen} />
+      <Stack.Screen name="question" component={QuestionScreen} />
     </Stack.Navigator>
   )
 }
@@ -50,5 +49,5 @@ export function MainNavigator() {
  *
  * `canExit` is used in ./app/app.tsx in the `useBackButtonHandler` hook.
  */
-const exitRoutes = ["welcome"]
+const exitRoutes = ["question"]
 export const canExit = (routeName: string) => exitRoutes.includes(routeName)
